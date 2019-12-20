74d4e653d8dc3e826f42a528348c3654c988f3eb
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-20 11:41:56 -0500

    built login with axios post and axios get in BubblePage

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/api/colors`)
+      .then(res => {
+        let getColors = res.data;
+        setColorList(getColors);
+      })
+      .catch(err => {
+        console.log("error getting colors", err.res);
+      });
+  }, []);
 
   return (
     <>
@@ -18,3 +29,6 @@ const BubblePage = () => {
 };
 
 export default BubblePage;
+
+// fetch your colors data from the server when the component mounts
+// set that data to the colorList state property
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
+  const [login, setLogin] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    e.preventDefault();
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .post(`/api/login`, login)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push(`/homepage`);
+      })
+      .catch(err => console.log(`error logging in`, err));
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <p>Login here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          className="login-input"
+          value={login.username}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="Password"
+          className="login-input"
+          value={login.password}
+          onChange={handleChange}
+        />
+        <button className="login-button">Login</button>
+      </form>
     </>
   );
 };
